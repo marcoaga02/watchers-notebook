6c9a4776e877ed113207a02fc061788e07f9a001
Marco Agatensi <marcoagatensi@gmail.com>
2026-08-21 12:42:16 +0200

    Updated notebook panel

--- Assets/Scripts/CreatureJournalRow.cs
+++ Assets/Scripts/CreatureJournalRow.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Localization;
 using UnityEngine.UI;
 
 public class CreatureJournalRow : MonoBehaviour
@@ -9,16 +10,19 @@ public class CreatureJournalRow : MonoBehaviour
     [SerializeField] private Image icon;
     [SerializeField] private TMP_Text nameLabel;
     [SerializeField] private TMP_Text implementsLabel;
+    [SerializeField] private LocalizedString nameLabelFormat;
+    [SerializeField] private LocalizedString implementsLabelFormat;
 
     public void Setup(CreatureDefinition species, IReadOnlyList<CapabilitySigil> collectedSigils)
     {
         icon.sprite = species.icon;
-        nameLabel.text = species.displayName.GetLocalizedString();
+        nameLabel.text = $"{nameLabelFormat.GetLocalizedString()} {species.displayName.GetLocalizedString()}";
 
         var implemented = collectedSigils.Where(species.Implements).ToList();
-
-        implementsLabel.text = implemented.Count == 0
+        var implementedText = implemented.Count == 0
             ? "???"
             : string.Join(", ", implemented.Select(sigil => sigil.interfaceName));
+
+        implementsLabel.text = $"{implementsLabelFormat.GetLocalizedString()} {implementedText}";
     }
 }
