8c4be30618053c36449323f2cbe5e5310546caf0
Marco Agatensi <marcoagatensi@gmail.com>
2026-08-24 18:58:46 +0200

    Ongoing scene changes

--- Assets/Scripts/CreatureAI.cs
+++ Assets/Scripts/CreatureAI.cs
@@ -5,19 +5,14 @@ public class CreatureAI : MonoBehaviour
 {
     [SerializeField] private Transform pointA;
     [SerializeField] private Transform pointB;
-    [SerializeField] private float playerBlockDistance = 1f;
-    [SerializeField] private float playerBlockWidth = 0.6f;
-    [SerializeField] private LayerMask playerMask;
 
     private CreatureMover _mover;
-    private Collider2D _collider;
     private Transform _target;
     private Vector2 _travelDirection;
 
     private void Awake()
     {
         _mover = GetComponent<CreatureMover>();
-        _collider = GetComponent<Collider2D>();
         _target = pointB;
         _travelDirection = ((Vector2)pointB.position - (Vector2)pointA.position).normalized;
     }
@@ -32,18 +27,6 @@ public class CreatureAI : MonoBehaviour
             toTarget = _target.position - transform.position;
         }
 
-        if (IsPlayerAhead(toTarget))
-        {
-            _mover.SetInput(Vector2.zero);
-            return;
-        }
-
         _mover.SetInput(toTarget);
-    }
-
-    private bool IsPlayerAhead(Vector2 direction)
-    {
-        var hit = Physics2D.CircleCast(_collider.bounds.center, playerBlockWidth, direction, playerBlockDistance, playerMask);
-        return hit.collider != null;
     }
 }
